8290d7627fc8e57f7dbc98ae617a5ed4754ef9af
Danbi Choi <sweetrain05@hotmail.com>
2023-07-15 22:46:45 -0700

    working on new page

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,19 +1,19 @@
 import aboutUsImage1 from "../assets/img/aboutUsImage1.jpg";
 import { motion } from "framer-motion";
 import motionWrapper from "../wrapper/motionWrapper";
+import { Link } from "react-router-dom";
 
-function NewsCard({ ascend }) {
+function NewsCard({ article }) {
     return (
-        <motion.article variants={ascend} className="card">
-            <img src={aboutUsImage1} alt={aboutUsImage1} />
+        <motion.article className="card">
+            <img src={article?.image_url} alt={article?.title} />
             <div className="text">
-                <h3>Lorem ipsum dolor sit amet</h3>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam non consectetur mauris. Ut eu dui nunc. Suspendisse
-                </p>
-                <h5>Published on July 4</h5>
-                <button>Read more ⇀</button>
+                <h3>{article?.title}</h3>
+                <p>{article?.subtitle}</p>
+                <h5>Published on {article?.published_at}</h5>
+                <Link to={article?.url} target="_blank">
+                    <button>Read more ⇀</button>
+                </Link>
             </div>
         </motion.article>
     );
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,22 +1,107 @@
-import { useEffect } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { motion } from "framer-motion";
 import motionWrapper from "../wrapper/motionWrapper";
 import NewsCard from "../components/NewsCard";
 import axios from "axios";
+import LoadingGIF from "../assets/img/Loading_icon.gif";
 
 function News({ ascend }) {
+    //state
+    const [articleList, setArticleList] = useState([]);
+    const [articleLength, setArticleLength] = useState(0);
+    const [articleInfo, setArticleInfo] = useState([]);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [loading, setLoading] = useState(true);
+    const [isInitialRender, setIsInitialRender] = useState(true);
+
     useEffect(() => {
         window.scrollTo(0, 0);
-        loadNews();
+        if (articleLength === 0) loadArticleList();
     }, []);
 
-    // const loadNews = async () => {
-    //     try {
-    //         const {data} = await axios.get()
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // }
+    const loadArticleList = async () => {
+        try {
+            const { associated_articles } = (
+                await axios.request({
+                    method: "GET",
+                    url: "https://medium2.p.rapidapi.com/user/37bdb09bd139/articles",
+                    headers: {
+                        "X-RapidAPI-Key":
+                            "6731250122msh22a4fc489d981d6p181ae7jsn42326f89d137",
+                        "X-RapidAPI-Host": "medium2.p.rapidapi.com",
+                    },
+                })
+            ).data;
+            setArticleList(associated_articles);
+            setArticleLength(associated_articles.length);
+            console.log("article list load complete.");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        if (!isInitialRender) {
+            console.log(articleList);
+            console.log(articleLength);
+            const articlesPerPage = 3;
+            const loadedArticleNumber = articlesPerPage * pageNumber;
+            if (articleList?.length > 0) {
+                if (
+                    articleLength <= articlesPerPage ||
+                    loadedArticleNumber < articleLength
+                ) {
+                    if (pageNumber === 1) {
+                        for (let i = 0; i < loadedArticleNumber; i++) {
+                            loadArticleInfo(i);
+                        }
+                    } else {
+                        for (
+                            let i = articlesPerPage;
+                            i < loadedArticleNumber;
+                            i++
+                        ) {
+                            loadArticleInfo(i);
+                        }
+                    }
+                    // setTimeout(() => {
+                    //     setLoading(false);
+                    // }, 3000);
+                }
+            }
+        } else {
+            setIsInitialRender(false);
+        }
+    }, [articleList, pageNumber, isInitialRender]);
+
+    const loadArticleInfo = async (i) => {
+        try {
+            const response = await axios.request({
+                method: "GET",
+                url: `https://medium2.p.rapidapi.com/article/${articleList[i]}`,
+                headers: {
+                    "X-RapidAPI-Key":
+                        "6731250122msh22a4fc489d981d6p181ae7jsn42326f89d137",
+                    "X-RapidAPI-Host": "medium2.p.rapidapi.com",
+                },
+            });
+            setArticleInfo((articleInfo) => [...articleInfo, response.data]);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        if (articleInfo?.length >= articleLength) {
+            setLoading(false);
+        }
+    }, [articleInfo]);
+
+    useEffect(() => {
+        if (articleInfo?.length > 0) {
+            console.log(articleInfo);
+        }
+    }, [articleInfo]);
 
     return (
         <article className="page news">
@@ -29,9 +114,17 @@ function News({ ascend }) {
                 </motion.p>
             </div>
             <div className="news_container">
-                <NewsCard />
-                <NewsCard />
-                <NewsCard />
+                {loading ? (
+                    <img src={LoadingGIF} alt="Loading..." />
+                ) : (
+                    articleInfo?.map((article, i) => (
+                        <NewsCard key={i} article={article} />
+                    ))
+                )}
+                {/* {articleInfo?.length > 0 &&
+                    articleInfo?.map((article) => (
+                        <NewsCard key={article.id} article={article} />
+                    ))} */}
             </div>
         </article>
     );
