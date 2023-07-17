6c0e38001f446e6f46fd1085e702a5944878ef56
Danbi Choi <sweetrain05@hotmail.com>
2023-07-17 15:48:01 -0700

    News page updated with article loading

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,59 +1,26 @@
-// import { Link } from "react-router-dom";
-// import moment from "moment";
-
-// function NewsCard({ article }) {
-//     return (
-//         <article className="card">
-//             <img
-//                 src={article?.cover_image}
-//                 alt={`Image related to ${article?.title}`}
-//             />
-//             <div className="text">
-//                 <h3>
-//                     {article?.title?.length > 50
-//                         ? article?.title.substring(0, 50) + "..."
-//                         : article?.title}
-//                 </h3>
-//                 <p>{article?.description}</p>
-//                 <h5>
-//                     Published on{" "}
-//                     {moment(article?.published_at).format("MMMM Do YYYY")}
-//                 </h5>
-//                 <Link to={article?.url} target="_blank">
-//                     <button>Read more ⇀</button>
-//                 </Link>
-//             </div>
-//         </article>
-//     );
-// }
-// export default NewsCard;
-
 import { Link } from "react-router-dom";
 import moment from "moment";
 
 function NewsCard({ article }) {
     return (
-        <article className="card">
-            <img
-                src={article?.image_url}
-                alt={`Image related to ${article?.title}`}
-            />
-            <div className="text">
-                <h3>
-                    {article?.title?.length > 50
-                        ? article?.title.substring(0, 50) + "..."
-                        : article?.title}
-                </h3>
-                <p>{article?.description}</p>
-                <h5>
-                    Published on{" "}
-                    {moment(article?.published_at).format("MMMM Do YYYY")}
-                </h5>
-                <Link to={article?.url} target="_blank">
+        <Link to={article?.url} target="_blank">
+            <article className="card">
+                <img src={article?.image_url} alt={`${article?.title}`} />
+                <div className="text">
+                    <h3>
+                        {article?.title?.length > 50
+                            ? article?.title.substring(0, 50) + "..."
+                            : article?.title}
+                    </h3>
+                    <h5>
+                        Published on{" "}
+                        {moment(article?.published_at).format("MMMM Do YYYY")}
+                    </h5>
+
                     <button>Read more ⇀</button>
-                </Link>
-            </div>
-        </article>
+                </div>
+            </article>
+        </Link>
     );
 }
 export default NewsCard;
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,9 +1,10 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import motionWrapper from "../wrapper/motionWrapper";
 import NewsCard from "../components/NewsCard";
 import axios from "axios";
 import LoadingGIF from "../assets/img/Loading_icon.gif";
+import { newsData, newsList } from "../data/newsData";
 
 function News({ ascend }) {
     //state
@@ -21,87 +22,99 @@ function News({ ascend }) {
 
     const loadArticleList = async () => {
         try {
-            const { associated_articles } = (
-                await axios.request({
-                    method: "GET",
-                    url: "https://medium2.p.rapidapi.com/user/37bdb09bd139/articles",
-                    headers: {
-                        "X-RapidAPI-Key":
-                            "6731250122msh22a4fc489d981d6p181ae7jsn42326f89d137",
-                        "X-RapidAPI-Host": "medium2.p.rapidapi.com",
-                    },
-                })
-            ).data;
-            setArticleList(associated_articles);
-            setArticleLength(associated_articles.length);
-            console.log("article list load complete.");
+            setTimeout(() => {
+                setArticleList(newsList);
+                setArticleLength(newsList.length);
+            }, 1000);
         } catch (error) {
             console.error(error);
         }
     };
 
+    // 👉 For real-time API request through axios, use the code below for loadArticleList()
+    // const loadArticleList = async () => {
+    //     try {
+    //         const { associated_articles } = (
+    //             await axios.request({
+    //                 method: "GET",
+    //                 url: "https://medium2.p.rapidapi.com/user/37bdb09bd139/articles",
+    //                 headers: {
+    //                     "X-RapidAPI-Key":
+    //                         "6731250122msh22a4fc489d981d6p181ae7jsn42326f89d137",
+    //                     "X-RapidAPI-Host": "medium2.p.rapidapi.com",
+    //                 },
+    //             })
+    //         ).data;
+    //         setArticleList(associated_articles);
+    //         setArticleLength(associated_articles.length);
+    //         console.log("article list load complete.");
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
+
     useEffect(() => {
         if (!isInitialRender) {
-            console.log(articleList);
-            console.log(articleLength);
             const articlesPerPage = 3;
             const loadedArticleNumber = articlesPerPage * pageNumber;
             if (articleList?.length > 0) {
-                if (
-                    articleLength <= articlesPerPage ||
-                    loadedArticleNumber < articleLength
-                ) {
-                    if (pageNumber === 1) {
-                        for (let i = 0; i < loadedArticleNumber; i++) {
-                            loadArticleInfo(i);
-                        }
-                    } else {
-                        for (
-                            let i = articlesPerPage;
-                            i < loadedArticleNumber;
-                            i++
-                        ) {
-                            loadArticleInfo(i);
-                        }
+                if (pageNumber === 1) {
+                    for (let i = 0; i < loadedArticleNumber; i++) {
+                        loadArticleInfo(i);
                     }
-                    // setTimeout(() => {
-                    //     setLoading(false);
-                    // }, 3000);
+                } else {
+                    for (
+                        let i = loadedArticleNumber - articlesPerPage;
+                        i < Math.min(loadedArticleNumber, articleLength);
+                        i++
+                    ) {
+                        loadArticleInfo(i);
+                    }
                 }
             }
         } else {
             setIsInitialRender(false);
         }
-    }, [articleList, pageNumber, isInitialRender]);
+    }, [articleList, pageNumber]);
 
-    const loadArticleInfo = async (i) => {
+    const loadArticleInfo = (i) => {
         try {
-            const response = await axios.request({
-                method: "GET",
-                url: `https://medium2.p.rapidapi.com/article/${articleList[i]}`,
-                headers: {
-                    "X-RapidAPI-Key":
-                        "6731250122msh22a4fc489d981d6p181ae7jsn42326f89d137",
-                    "X-RapidAPI-Host": "medium2.p.rapidapi.com",
-                },
-            });
-            setArticleInfo((articleInfo) => [...articleInfo, response.data]);
+            setTimeout(() => {
+                setArticleInfo((articleInfo) => [...articleInfo, newsData[i]]);
+            }, 1000);
         } catch (err) {
             console.log(err);
         }
     };
 
+    // 👉 For real-time API request through axios, use the code below for loadArticleInfo()
+    // const loadArticleInfo = async (i) => {
+    //     try {
+    //         const response = await axios.request({
+    //             method: "GET",
+    //             url: `https://medium2.p.rapidapi.com/article/${articleList[i]}`,
+    //             headers: {
+    //                 "X-RapidAPI-Key":
+    //                     "6731250122msh22a4fc489d981d6p181ae7jsn42326f89d137",
+    //                 "X-RapidAPI-Host": "medium2.p.rapidapi.com",
+    //             },
+    //         });
+    //         setArticleInfo((articleInfo) => [...articleInfo, response.data]);
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // };
+
     useEffect(() => {
-        if (articleInfo?.length >= articleLength) {
+        if (articleInfo?.length > 0) {
             setLoading(false);
         }
     }, [articleInfo]);
 
-    useEffect(() => {
-        if (articleInfo?.length > 0) {
-            console.log(articleInfo);
-        }
-    }, [articleInfo]);
+    const handleLoadMore = () => {
+        setPageNumber((pageNum) => pageNum + 1);
+        setLoading(true);
+    };
 
     return (
         <article className="page news">
@@ -114,17 +127,17 @@ function News({ ascend }) {
                 </motion.p>
             </div>
             <div className="news_container">
-                {loading ? (
-                    <img src={LoadingGIF} alt="Loading..." />
-                ) : (
-                    articleInfo?.map((article, i) => (
-                        <NewsCard key={i} article={article} />
-                    ))
-                )}
-                {/* {articleInfo?.length > 0 &&
-                    articleInfo?.map((article) => (
-                        <NewsCard key={article.id} article={article} />
-                    ))} */}
+                {!loading || articleInfo?.length > 0
+                    ? articleInfo?.map((article) => (
+                          <NewsCard key={article.id} article={article} />
+                      ))
+                    : null}
+                {loading && <img src={LoadingGIF} alt="Loading..." />}
+                {!loading && articleInfo?.length < articleLength ? (
+                    <button className="loadBtn" onClick={handleLoadMore}>
+                        Load More
+                    </button>
+                ) : null}
             </div>
         </article>
     );
