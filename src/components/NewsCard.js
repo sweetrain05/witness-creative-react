8a0baaf6c5e68939fd12d211c2804bf45b02c60e
Danbi Choi <sweetrain05@hotmail.com>
2023-07-16 23:31:21 -0700

    news card modified(moment applied)

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,21 +1,59 @@
-import aboutUsImage1 from "../assets/img/aboutUsImage1.jpg";
-import { motion } from "framer-motion";
-import motionWrapper from "../wrapper/motionWrapper";
+// import { Link } from "react-router-dom";
+// import moment from "moment";
+
+// function NewsCard({ article }) {
+//     return (
+//         <article className="card">
+//             <img
+//                 src={article?.cover_image}
+//                 alt={`Image related to ${article?.title}`}
+//             />
+//             <div className="text">
+//                 <h3>
+//                     {article?.title?.length > 50
+//                         ? article?.title.substring(0, 50) + "..."
+//                         : article?.title}
+//                 </h3>
+//                 <p>{article?.description}</p>
+//                 <h5>
+//                     Published on{" "}
+//                     {moment(article?.published_at).format("MMMM Do YYYY")}
+//                 </h5>
+//                 <Link to={article?.url} target="_blank">
+//                     <button>Read more ⇀</button>
+//                 </Link>
+//             </div>
+//         </article>
+//     );
+// }
+// export default NewsCard;
+
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 function NewsCard({ article }) {
     return (
-        <motion.article className="card">
-            <img src={article?.image_url} alt={article?.title} />
+        <article className="card">
+            <img
+                src={article?.image_url}
+                alt={`Image related to ${article?.title}`}
+            />
             <div className="text">
-                <h3>{article?.title}</h3>
-                <p>{article?.subtitle}</p>
-                <h5>Published on {article?.published_at}</h5>
+                <h3>
+                    {article?.title?.length > 50
+                        ? article?.title.substring(0, 50) + "..."
+                        : article?.title}
+                </h3>
+                <p>{article?.description}</p>
+                <h5>
+                    Published on{" "}
+                    {moment(article?.published_at).format("MMMM Do YYYY")}
+                </h5>
                 <Link to={article?.url} target="_blank">
                     <button>Read more ⇀</button>
                 </Link>
             </div>
-        </motion.article>
+        </article>
     );
 }
-export default motionWrapper(NewsCard);
+export default NewsCard;
